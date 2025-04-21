416ff5e0da9e8413c33d2850b0bc1d2aa37df4d1
Themi Megas <Tcm4760@gmail.com>
2025-04-21 01:38:33 -0500

    init commit, working very simple mca decoder

---
+++ simple mca decoder.py
@@ -0,0 +1,29 @@
+import zlib
+
+f = open("in.mca", "rb")
+
+chunk_offsets = []
+
+for i in range(1024):
+    offset_bytes = f.read(3)
+    offset = int.from_bytes(offset_bytes, "big")
+    f.seek(f.tell() + 1)
+    chunk_offsets.append(offset)
+
+
+chunks = []
+
+for chunk_offset in chunk_offsets:
+    if chunk_offset == 0:
+        continue
+
+    f.seek(chunk_offset * 4096)
+    chunk_length = int.from_bytes(f.read(4), "big")
+
+    f.seek(f.tell() + 1)
+    chunk_bytes = f.read(chunk_length - 1)
+
+    chunk_uncompressed = zlib.decompress(chunk_bytes)
+    chunks.append(chunk_uncompressed)
+
+print(chunks)
